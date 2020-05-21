2798491c7e033bbedc430f9d1690deca22ad1239
Rodrigo <rodrigosalvate@gmail.com>
2020-05-21 17:16:39 -0300

    Adding course test
    Adding course test

---
+++ CursoOnline/Test/CursoOnline.DomainTest/Courses/CourseTest.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace CursoOnline.DomainTest.Courses
+{
+    public class CourseTest
+    {
+        [Fact]
+        public void MustCreateCourse()
+        {
+            //Arrange
+            const string name = "English and Programming";
+            const double courseLoad = 80;
+            const string targetAudience = "Programmers";
+            const double value = 895;
+
+            //Act
+            var course = new Courses(name, courseLoad, targetAudience, value);
+
+            //Assert
+            Assert.Equal(name, course.Name);
+            Assert.Equal(courseLoad, course.CourseLoad);
+            Assert.Equal(targetAudience, course.TargetAudience);
+            Assert.Equal(value, course.Value);
+        }
+
+        public class Courses
+        {
+            public string Name { get; private set; }
+            public double CourseLoad { get; private set; }
+            public string TargetAudience { get; private set; }
+            public double Value { get; private set; }
+
+            public Courses(string name, double courseLoad, string targetAudience, double value)
+            {
+                Name = name;
+                CourseLoad = courseLoad;
+                TargetAudience = targetAudience;
+                Value = value;
+            }
+        }
+    }
+}
